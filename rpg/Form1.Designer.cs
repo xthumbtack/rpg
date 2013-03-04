8190c207908f732572006ea87283878b2176e76d
Ryanh Somphithack <ry.somphithack@gmail.com>
2013-03-03 18:27:12 -0800

    Revert "Revert "first upload""
    This reverts commit 9ed061aef6138a9ff7391b5c97f1f6e61fa34108.

---
+++ rpg/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace rpg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start_button = new System.Windows.Forms.Button();
+            this.exit_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start_button
+            // 
+            this.start_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_button.ForeColor = System.Drawing.Color.White;
+            this.start_button.Location = new System.Drawing.Point(309, 246);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(75, 23);
+            this.start_button.TabIndex = 0;
+            this.start_button.Text = "Start";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // exit_button
+            // 
+            this.exit_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit_button.ForeColor = System.Drawing.Color.White;
+            this.exit_button.Location = new System.Drawing.Point(309, 297);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(75, 23);
+            this.exit_button.TabIndex = 1;
+            this.exit_button.Text = "Exit";
+            this.exit_button.UseVisualStyleBackColor = true;
+            this.exit_button.Click += new System.EventHandler(this.exit_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(261, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 73);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "RPG";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(692, 470);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exit_button);
+            this.Controls.Add(this.start_button);
+            this.Name = "Form1";
+            this.Text = "RPG(form1)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Button exit_button;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ rpg/Objects.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rpg
+{
+    class Objects
+    {
+        int str = 10, agl = 10, intel = 10, HP = 100, MP = 100;
+        int lvl = 1, exp = 0;
+
+        public void Ryanh()
+        {
+            int[] ryanhStats = new int[5] { str + 10, agl, intel, HP, MP };
+            int[] ryanhLvl = new int[2] { lvl, exp };
+        }
+        public void Matt()
+        {
+            int[] mattStats = new int[5] { str, agl, intel + 10, HP, MP };
+            int[] mattLvl = new int[2] { lvl, exp };
+        }
+        public void Aung()
+        {
+            int[] aungStats = new int[5] { str, agl + 10, intel, HP, MP };
+            int[] aungLvl = new int[2] { lvl, exp };
+        }
+
+        public void slime()
+        {
+            int slimeATK = 15, slimeHP = 80, slimeDEF = 10;
+        }
+
+        public void thief()
+        {
+            int thiefATK = 20, thiefHP = 120, thiefDEf = 15, thiefMP = 50;
+        }
+
+
+    }
+}
