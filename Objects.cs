25e165b212d063837e2d2e3cee4fcb5951350f8f
Ryanh Somphithack <ry.somphithack@gmail.com>
2013-03-03 20:54:06 -0800

    blah

---
+++ Objects.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rpg
+{
+   public static class Objects
+    {
+       public int str = 10, agl = 10, intel = 10, HP = 100, MP = 100;
+       public int lvl = 1, exp = 0;
+
+       public void Ryanh()
+       {
+           int[] stats = new int[5] { str + 10, agl, intel, HP, MP };
+           int[] level = new int[2] { lvl, exp };
+       }
+       public void Matt()
+       {
+           int[] stats = new int[5] { str, agl, intel + 10, HP, MP };
+           int[] level = new int[2] { lvl, exp };
+       }
+       public void Aung()
+       {
+           int[] stats = new int[5] { str, agl + 10, intel, HP, MP };
+           int[] level = new int[2] { lvl, exp };
+       }
+
+       public void slime()
+       {
+           int slimeATK = 15, slimeHP = 80, slimeDEF = 10;
+       }
+
+       public void thief()
+       {
+           int thiefATK = 20, thiefHP = 120, thiefDEf = 15, thiefMP = 50;
+       }
+
+    }
+}
---
+++ battle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using rpg.Objects;
+
+namespace rpg
+{
+    public partial class battle : Form
+    {
+        public battle()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void battle_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void load_enemy()
+        {
+            
+        }
+    }  
+}
